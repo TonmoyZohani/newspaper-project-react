863d9ef9244391401ac4d978873b120c23d3617e
TonmoyZohani <tonmoyzohani@gmail.com>
2023-06-14 23:02:23 +0600

    1st part done

--- src/components/LeftNav.jsx
+++ src/components/LeftNav.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const LeftNav = () => {
-  return (
-    <div>LeftNav</div>
-  )
-}
+  const [categories, setCategories] = useState([]);
 
-export default LeftNav
+  useEffect(() => {
+    fetch("http://localhost:5000/categories")
+      .then((res) => res.json())
+      .then((data) => {
+        setCategories(data);
+        console.log(data);
+      })
+      .catch((error) => console.log(console.error(error)));
+  }, []);
+
+  console.log(categories);
+
+  return (
+    <div>
+      <h1 className="text-xl font-semibold">All Category</h1>
+
+      <div className="space-y-2 mt-4">
+        {categories.map((category) => (
+          <p key={category.id}>
+            <Link to={`/categoty/${category.id}`}>{category.name}</Link>
+          </p>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default LeftNav;
